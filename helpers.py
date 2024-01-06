51bae9d5883ad189f7026ebbdbcf010aeb6d9add
NiinaNieminen <56301290+NiinaNieminen@users.noreply.github.com>
2024-01-06 13:29:15 +0200

    code improvements

--- helpers.py
+++ helpers.py
@@ -1,11 +1,7 @@
 import pandas as pd
-import json
+
 
 def make_sure_pd_index_timestamp(tseries):
     if not (type(tseries.first_valid_index()) == pd._libs.tslibs.timestamps.Timestamp):
         tseries.index = pd.to_datetime(tseries.index)
     return tseries
-
-def load_dict_from_json(filename):
-    data = json.load(open(filename,'r'))
-    return data
